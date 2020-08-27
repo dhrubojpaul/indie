d23d11445a393a9f92088ab46787f6d69cb62408
dhrubojpaul <dhrubojpaul@gmail.com>
2020-08-27 08:03:14 -0400

    logo

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   pwa: {
     name: 'INDIE'
-  }
+  },
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+            args[0].title = "INDIE Lab";
+            return args;
+        })
+}
 }
